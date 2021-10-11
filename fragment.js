25a1a042d781ff6dac12ab0ab4a066c52a76432d
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2021-10-12 01:18:03 +0530

    Add files via upload

---
+++ fragment.js
@@ -0,0 +1,13 @@
+// React Fragment in react js
+
+
+import React from "react";
+import ReactDOM from 'react-dom';
+ ReactDOM.render(
+   <React.Fragment>
+     <h1> this is fragment</h1>
+     <p1> how to use fragmne</p1>
+     <h2> this is fragment</h2>
+   </React.Fragment>,
+   document.getElementById("root")
+ );
