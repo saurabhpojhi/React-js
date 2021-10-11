0f2d2dc6adc1aad5c5d42163750bdbc13fdf1d54
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2021-10-12 01:17:41 +0530

    Add files via upload

---
+++ multipleRender.js
@@ -0,0 +1,12 @@
+// this is multiple  render elements inside reactdom.render
+
+import React from "react";
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div> 
+  <h1> react of render</h1>,
+  <p> this is saurbh gupta </p>
+  <h2> this is h2 </h2>
+  </div>,
+  document.getElementById("root"));
