71f78abc73031b27a95ed813021a80ba55b42478
saurabh kumar <35069331+saurabhpojhi@users.noreply.github.com>
2021-10-21 11:04:20 +0530

    Add files via upload

---
+++ componetReact/componets/Lists.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Lists(){
+    return(
+    <ol>
+        <li> saurabh</li>
+        <li> gupta</li>
+        <li> parsa</li>
+        <li> saran</li>
+      </ol>);
+}
+export default Lists;
